63d923130c2a472b849b213508ac0441b623610c
David Dal Busco <david.dalbusco@outlook.com>
2024-07-17 07:24:37 +0200

    feat: init signer class

--- src/signer.ts
+++ src/signer.ts
@@ -1,1 +1,35 @@
-export default {};
+import {
+  IcrcWalletPermissionsRequestType,
+  IcrcWalletRequestPermissionsRequestType,
+  IcrcWalletSupportedStandardsRequestType
+} from './types/icrc-requests';
+
+interface SignerParameters {}
+
+export class Signer {
+  #walletOrigin: string | undefined;
+
+  private constructor({}: SignerParameters) {
+    window.addEventListener('message', this.onMessage);
+  }
+
+  static connect(parameters: SignerParameters): Signer {
+    const signer = new Signer(parameters);
+    return signer;
+  }
+
+  disconnect = () => {
+    window.removeEventListener('message', this.onMessage);
+  };
+
+  private onMessage = async ({
+    data,
+    origin
+  }: MessageEvent<
+    Partial<
+      | IcrcWalletRequestPermissionsRequestType
+      | IcrcWalletPermissionsRequestType
+      | IcrcWalletSupportedStandardsRequestType
+    >
+  >) => {};
+}
