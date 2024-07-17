9fb71d3be29dd5ea0a2526cc450b9994c45840d0
David Dal Busco <david.dalbusco@outlook.com>
2024-07-17 08:27:12 +0200

    feat: init signer class (#6)

--- src/signer.ts
+++ src/signer.ts
@@ -1,1 +1,39 @@
-export default {};
+import type {
+  IcrcWalletPermissionsRequestType,
+  IcrcWalletRequestPermissionsRequestType,
+  IcrcWalletSupportedStandardsRequestType
+} from './types/icrc-requests';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SignerParameters {}
+
+type SignerMessageEvent = MessageEvent<
+  Partial<
+    | IcrcWalletRequestPermissionsRequestType
+    | IcrcWalletPermissionsRequestType
+    | IcrcWalletSupportedStandardsRequestType
+  >
+>;
+
+export class Signer {
+  readonly #walletOrigin: string | undefined;
+
+  private constructor(_parameters: SignerParameters) {
+    window.addEventListener('message', this.onMessageListener);
+  }
+
+  static connect(parameters: SignerParameters): Signer {
+    const signer = new Signer(parameters);
+    return signer;
+  }
+
+  disconnect = (): void => {
+    window.removeEventListener('message', this.onMessageListener);
+  };
+
+  private readonly onMessageListener = (message: SignerMessageEvent): void => {
+    void this.onMessage(message);
+  };
+
+  private readonly onMessage = async (_message: SignerMessageEvent): Promise<void> => {};
+}
