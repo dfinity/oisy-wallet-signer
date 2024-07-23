b31de2f450d266b34749b79acca29cbb22d48c80
David Dal Busco <david.dalbusco@outlook.com>
2024-07-23 09:35:01 +0200

    feat: use library in demo and init wallet and signer (#16)

--- demo/src/wallet_frontend/src/routes/+page.svelte
+++ demo/src/wallet_frontend/src/routes/+page.svelte
@@ -1,1 +1,17 @@
+<script lang="ts">
+    import {notSignedIn} from "$core/derived/auth.derived";
+    import {Signer} from "@dfinity/oisy-wallet-signer/signer";
+
+    let signer: Signer | undefined;
+
+    $effect(() => {
+        if ($notSignedIn) {
+            signer?.disconnect();
+            return;
+        }
+
+        signer = Signer.connect({})
+    });
+</script>
+
 <h1>Wallet Frontend</h1>
