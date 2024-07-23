3e861da9a87bd8257064a85b7fd6cac9cd87c677
David Dal Busco <david.dalbusco@outlook.com>
2024-07-23 13:15:36 +0200

    test: login and connect (#19)

--- e2e/connect.spec.ts
+++ e2e/connect.spec.ts
@@ -1,4 +1,5 @@
-import {testWithII} from '@dfinity/internet-identity-playwright';
+import {InternetIdentityPage, testWithII} from '@dfinity/internet-identity-playwright';
+import {expect} from '@playwright/test';
 
 testWithII.beforeEach(async ({iiPage, browser}) => {
   const REPLICA_URL = 'http://localhost:4943';
@@ -9,8 +10,30 @@ testWithII.beforeEach(async ({iiPage, browser}) => {
 
 const RELYING_PARTY_URL = 'http://localhost:5173';
 
-testWithII('should sign-in relying-party with a new user', async ({page, iiPage}) => {
-  await page.goto(RELYING_PARTY_URL);
+testWithII(
+  'should sign-in relying-party with a new user',
+  async ({page: partyPage, iiPage: partyIIPage, context, browser}) => {
+    await partyPage.goto(RELYING_PARTY_URL);
 
-  await iiPage.signInWithNewIdentity();
-});
+    const identity = await partyIIPage.signInWithNewIdentity();
+
+    await expect(partyPage.getByTestId('connect-wallet-button')).toBeVisible();
+
+    const walletPagePromise = context.waitForEvent('page');
+
+    await partyPage.getByTestId('connect-wallet-button').click();
+
+    const walletPage = await walletPagePromise;
+    await expect(walletPage).toHaveTitle('Wallet');
+
+    await expect(walletPage.getByTestId('login-button')).toBeVisible();
+
+    const walletIIPage = new InternetIdentityPage({
+      page: walletPage,
+      context,
+      browser
+    });
+
+    await walletIIPage.signInWithIdentity({identity});
+  }
+);
