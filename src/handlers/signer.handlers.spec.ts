94efdc7ef292e7fdc360c7490773caf227118cdd
David Dal Busco <david.dalbusco@outlook.com>
2024-07-30 14:21:24 +0200

    feat: signer handlers notify ready

---
+++ src/handlers/signer.handlers.spec.ts
@@ -0,0 +1,41 @@
+import type {Mock} from 'vitest';
+import type {IcrcReadyResponseType} from '../types/icrc-responses';
+import {JSON_RPC_VERSION_2, type RpcIdType} from '../types/rpc';
+import {notifyReady} from './signer.handlers';
+
+describe('Signer handlers', () => {
+  let originalOpener: typeof window.opener;
+
+  let postMessageMock: Mock;
+
+  beforeEach(() => {
+    originalOpener = window.opener;
+
+    postMessageMock = vi.fn();
+
+    vi.stubGlobal('opener', {postMessage: postMessageMock});
+  });
+
+  afterEach(() => {
+    window.opener = originalOpener;
+
+    vi.restoreAllMocks();
+  });
+
+  describe('notifyReady', () => {
+    it('should post a message with the msg', () => {
+      const id: RpcIdType = 'test-123';
+      const origin = 'https://hello.com';
+
+      notifyReady({id, origin});
+
+      const expectedMessage: IcrcReadyResponseType = {
+        jsonrpc: JSON_RPC_VERSION_2,
+        id,
+        result: 'ready'
+      };
+
+      expect(postMessageMock).toHaveBeenCalledWith(expectedMessage, origin);
+    });
+  });
+});
