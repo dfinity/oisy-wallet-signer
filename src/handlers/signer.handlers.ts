1026608164a17d747dfc8c7a7701e270af3484a2
David Dal Busco <david.dalbusco@outlook.com>
2024-07-31 12:25:51 +0200

    feat: notify error for signer

--- src/handlers/signer.handlers.ts
+++ src/handlers/signer.handlers.ts
@@ -1,12 +1,42 @@
 import type {IcrcReadyResponseType} from '../types/icrc-responses';
-import {JSON_RPC_VERSION_2, type RpcIdType} from '../types/rpc';
+import {
+  JSON_RPC_VERSION_2,
+  type RpcIdType,
+  type RpcResponseErrorType,
+  type RpcResponseType,
+  type RpcResponseWithErrorType
+} from '../types/rpc';
 
-export const notifyReady = ({id, origin}: {id: RpcIdType; origin: string}): void => {
+interface Notify {
+  id: RpcIdType;
+  origin: string;
+}
+
+export const notifyReady = ({id, origin}: Notify): void => {
   const msg: IcrcReadyResponseType = {
     jsonrpc: JSON_RPC_VERSION_2,
     id,
     result: 'ready'
   };
 
-  window.opener.postMessage(msg, origin);
+  notify({msg, origin});
 };
+
+export const notifyError = ({
+  id,
+  error,
+  origin
+}: {
+  error: RpcResponseErrorType;
+} & Notify): void => {
+  const msg: RpcResponseWithErrorType = {
+    jsonrpc: JSON_RPC_VERSION_2,
+    id,
+    error
+  };
+
+  notify({msg, origin});
+};
+
+const notify = ({msg, origin}: {msg: RpcResponseType} & Pick<Notify, 'origin'>): void =>
+  window.opener.postMessage(msg, origin);
