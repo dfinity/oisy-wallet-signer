022d335a98ba542a0d95751fcba7afd79a82d2db
David Dal Busco <david.dalbusco@outlook.com>
2024-08-05 07:44:44 +0200

    chore: format

--- demo/src/wallet_frontend/src/routes/+page.svelte
+++ demo/src/wallet_frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { notSignedIn } from '$core/derived/auth.derived';
 	import { Signer } from '@dfinity/oisy-wallet-signer/signer';
-    import UserId from '$core/components/UserId.svelte';
+	import UserId from '$core/components/UserId.svelte';
 
 	let signer: Signer | undefined;
 
