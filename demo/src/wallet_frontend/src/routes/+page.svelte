76f083da5d741e46d998a07d5331b0856eb9ab93
David Dal Busco <david.dalbusco@outlook.com>
2024-08-09 11:52:20 +0200

    feat: expose icrc constants

--- demo/src/wallet_frontend/src/routes/+page.svelte
+++ demo/src/wallet_frontend/src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { notSignedIn } from '$core/derived/auth.derived';
 	import { Signer } from '@dfinity/oisy-wallet-signer/signer';
 	import UserId from '$core/components/UserId.svelte';
+    import {ICRC25_REQUEST_PERMISSIONS} from "@dfinity/oisy-wallet-signer";
 
 	let signer: Signer | undefined;
 
@@ -17,6 +18,9 @@
 			signer?.disconnect();
 		};
 	});
+
+    // TODO: to be removed.
+    console.log(ICRC25_REQUEST_PERMISSIONS);
 </script>
 
 <UserId />
