d66b886980b2586f4a308c1f4b09c122a864b5a7
David Dal Busco <david.dalbusco@outlook.com>
2024-08-09 09:25:11 +0200

    chore: format

--- demo/src/wallet_frontend/src/routes/+page.svelte
+++ demo/src/wallet_frontend/src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-    import {notSignedIn} from '$core/derived/auth.derived';
-    import {Signer} from '@dfinity/oisy-wallet-signer/signer';
-    import UserId from '$core/components/UserId.svelte';
-    import {ICRC25_REQUEST_PERMISSIONS, type IcrcWalletScopesParams} from "@dfinity/oisy-wallet-signer";
+	import { notSignedIn } from '$core/derived/auth.derived';
+	import { Signer } from '@dfinity/oisy-wallet-signer/signer';
+	import UserId from '$core/components/UserId.svelte';
+	import {
+		ICRC25_REQUEST_PERMISSIONS,
+		type IcrcWalletScopesParams
+	} from '@dfinity/oisy-wallet-signer';
 
-    let signer: Signer | undefined;
+	let signer: Signer | undefined;
 
 	$effect(() => {
 		if ($notSignedIn) {
@@ -14,15 +17,15 @@
 
 		signer = Signer.init({});
 
-        const unsubscribeRequestPermissions = signer.on({
-            method: ICRC25_REQUEST_PERMISSIONS,
-            callback: (data: IcrcWalletScopesParams) => {
-                console.log('TODO: Handle permissions requests:', data);
-            }
-        });
+		const unsubscribeRequestPermissions = signer.on({
+			method: ICRC25_REQUEST_PERMISSIONS,
+			callback: (data: IcrcWalletScopesParams) => {
+				console.log('TODO: Handle permissions requests:', data);
+			}
+		});
 
-        // I can unsubscribe just one listener.
-        setTimeout(() => unsubscribeRequestPermissions(), 2000);
+		// I can unsubscribe just one listener.
+		setTimeout(() => unsubscribeRequestPermissions(), 2000);
 
 		return () => {
 			signer?.disconnect();
