34531859e4402a8dcdcec17b91463ce58ed9ae18
David Dal Busco <david.dalbusco@outlook.com>
2024-08-12 21:28:23 +0200

    feat: icrc-49 constants

--- src/constants/icrc.constants.ts
+++ src/constants/icrc.constants.ts
@@ -4,6 +4,8 @@ export const ICRC25_SUPPORTED_STANDARDS = 'icrc25_supported_standards';
 // TODO: ICRC27 to be defined
 export const ICRC27_ACCOUNTS = 'icrc27_accounts';
 export const ICRC29_STATUS = 'icrc29_status';
+// TODO: ICRC49 to be defined
+export const ICRC49_CALL_CANISTER = 'icrc49_call_canister';
 
 export const ICRC25_PERMISSION_GRANTED = 'granted';
 export const ICRC25_PERMISSION_DENIED = 'denied';
@@ -12,3 +14,4 @@ export const ICRC25_PERMISSION_ASK_ON_USE = 'ask_on_use';
 export const ICRC25 = 'ICRC-25';
 export const ICRC27 = 'ICRC-27';
 export const ICRC29 = 'ICRC-29';
+export const ICRC49 = 'ICRC-49';
