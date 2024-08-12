b2732144d8587524fe0b82dbf4868b3a991d3373
David Dal Busco <david.dalbusco@outlook.com>
2024-08-12 14:49:24 +0200

    feat: notifyPermissionScopes (#69)

--- src/handlers/signer.handlers.ts
+++ src/handlers/signer.handlers.ts
@@ -1,5 +1,10 @@
 import {SIGNER_SUPPORTED_STANDARDS} from '../constants/signer.constants';
-import type {IcrcReadyResponse, IcrcSupportedStandardsResponse} from '../types/icrc-responses';
+import type {
+  IcrcReadyResponse,
+  IcrcScope,
+  IcrcScopesResponse,
+  IcrcSupportedStandardsResponse
+} from '../types/icrc-responses';
 import {
   JSON_RPC_VERSION_2,
   type RpcId,
@@ -30,6 +35,20 @@ export const notifySupportedStandards = ({id, origin}: Notify): void => {
     result: {
       supportedStandards: SIGNER_SUPPORTED_STANDARDS
     }
+  };
+
+  notify({msg, origin});
+};
+
+export const notifyPermissionScopes = ({
+  id,
+  origin,
+  scopes
+}: Notify & {scopes: IcrcScope[]}): void => {
+  const msg: IcrcScopesResponse = {
+    jsonrpc: JSON_RPC_VERSION_2,
+    id,
+    result: {scopes}
   };
 
   notify({msg, origin});
