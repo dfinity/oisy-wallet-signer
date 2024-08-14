9d37e5a2091f104a115027722231833508275fe4
David Dal Busco <david.dalbusco@outlook.com>
2024-08-14 09:25:36 +0200

    chore: format

--- demo/src/wallet_frontend/src/routes/+page.svelte
+++ demo/src/wallet_frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { notSignedIn } from '$core/derived/auth.derived';
 	import { Signer } from '@dfinity/oisy-wallet-signer/signer';
 	import UserId from '$core/components/UserId.svelte';
-    import ConfirmPermissions from '$lib/ConfirmPermissions.svelte';
+	import ConfirmPermissions from '$lib/ConfirmPermissions.svelte';
 	import { authStore } from '$core/stores/auth.store';
 	import { isNullish } from '@dfinity/utils';
 
