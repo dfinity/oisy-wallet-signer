4cdf427ab4208e5b7d3ca3744df4be1987ad9a33
David Dal Busco <david.dalbusco@outlook.com>
2024-08-14 08:27:21 +0200

    refactor: extract type scopes array (#80)

--- src/types/signer-subscribers.ts
+++ src/types/signer-subscribers.ts
@@ -1,10 +1,10 @@
 import {z} from 'zod';
-import {IcrcScopeSchema} from './icrc-responses';
+import {IcrcScopesArraySchema} from './icrc-responses';
 import {RpcIdSchema} from './rpc';
 
 export const RequestPermissionPayloadSchema = z.object({
   requestId: RpcIdSchema,
-  scopes: z.array(IcrcScopeSchema)
+  scopes: IcrcScopesArraySchema
 });
 
 export type RequestPermissionPayload = z.infer<typeof RequestPermissionPayloadSchema>;
