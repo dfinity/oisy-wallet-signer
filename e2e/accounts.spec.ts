ce5ffa96aec3598db88c0c73f6d1f1f236858372
David Dal Busco <david.dalbusco@dfinity.org>
2024-08-27 07:41:07 +0200

    test: rename and todo

--- e2e/accounts.spec.ts
+++ e2e/accounts.spec.ts
@@ -3,8 +3,12 @@ import {initTestSuite} from './utils/init.utils';
 
 const getPartyPage = initTestSuite();
 
-testWithII('should request permissions', async () => {
+testWithII('should requests permissions and list accounts', async () => {
   const partyPage = getPartyPage();
 
   await partyPage.accounts();
+});
+
+testWithII('should not requests permissions and list accounts', async () => {
+  // TODO
 });
