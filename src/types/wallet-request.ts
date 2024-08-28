cd21ed4decd62ec8948d085c456b174e534bd580
David Dal Busco <david.dalbusco@dfinity.org>
2024-08-28 17:02:17 +0200

    feat: wallet call canister arg and argType params

--- src/types/wallet-request.ts
+++ src/types/wallet-request.ts
@@ -1,4 +1,7 @@
+import {Type} from '@dfinity/candid/lib/cjs/idl';
+import {nonNullish} from '@dfinity/utils';
 import {z} from 'zod';
+import {IcrcCallCanisterRequestParamsSchema} from './icrc-requests';
 import {RpcIdSchema} from './rpc';
 
 export const WalletRequestOptionsTimeoutSchema = z.object({
@@ -29,3 +32,36 @@ export const WalletRequestOptionsWithTimeoutSchema = WalletRequestOptionsSchema.
 }).merge(WalletRequestOptionsTimeoutSchema);
 
 export type WalletRequestOptionsWithTimeout = z.infer<typeof WalletRequestOptionsWithTimeoutSchema>;
+
+/**
+ * Creates an extended schema for ICRC call canister request parameters.
+ *
+ * This function generates a schema that extends the base `IcrcCallCanisterRequestParamsSchema`
+ * by replacing the `arg` field with a custom type `T` and adding an `argType` field that must be
+ * an instance of the `Type` class from `@dfinity/candid`.
+ *
+ * @template T - The type of the `arg` field.
+ * @returns {z.ZodObject} - A Zod schema object that includes the base fields and the custom `arg` and `argType`.
+ */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const extendIcrcCallCanisterRequestParamsSchema = <T>() => {
+  return IcrcCallCanisterRequestParamsSchema.omit({arg: true}).extend({
+    arg: z.custom<T>().refine(nonNullish, {
+      message: 'arg is required'
+    }),
+    argType: z.instanceof(Type) as z.ZodType<Type>
+  });
+};
+
+/**
+ * Represents the type of parameters used in a wallet call, based on the
+ * extended ICRC call canister request schema.
+ *
+ * This type is inferred from the return type of `extendIcrcCallCanisterRequestParamsSchema<T>`,
+ * meaning it includes all fields of `IcrcCallCanisterRequestParamsSchema` with a generic `arg` type.
+ *
+ * @template T - The type of the `arg` field in the schema.
+ */
+export type WalletCallParams<T> = z.infer<
+  ReturnType<typeof extendIcrcCallCanisterRequestParamsSchema<T>>
+>;
