d76a9506b9d717d0c33443958a3d6b692c261e00
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-02 08:21:37 +0200

    chore: format

--- demo/src/core/stores/auth.store.ts
+++ demo/src/core/stores/auth.store.ts
@@ -1,8 +1,7 @@
 import {
 	AUTH_MAX_TIME_TO_LIVE,
 	AUTH_POPUP_HEIGHT,
-	AUTH_POPUP_WIDTH,
-	INTERNET_IDENTITY_CANISTER_ID
+	AUTH_POPUP_WIDTH
 } from '$core/constants/app.constants';
 import type { OptionIdentity } from '$core/types/identity';
 import { createAuthClient } from '$core/utils/auth.utils';
@@ -57,11 +56,12 @@ const initAuthStore = (): AuthStore => {
 				const container = import.meta.env.VITE_CONTAINER;
 				const iiId = import.meta.env.VITE_INTERNET_IDENTITY_ID;
 
-				const identityProvider = nonNullish(container) && nonNullish(iiId)
-					? /apple/i.test(navigator?.vendor)
-						? `http://localhost:5987?canisterId=${iiId}`
-						: `http://${iiId}.localhost:5987`
-					: `https://identity.${domain ?? 'ic0.app'}`;
+				const identityProvider =
+					nonNullish(container) && nonNullish(iiId)
+						? /apple/i.test(navigator?.vendor)
+							? `http://localhost:5987?canisterId=${iiId}`
+							: `http://${iiId}.localhost:5987`
+						: `https://identity.${domain ?? 'ic0.app'}`;
 
 				await authClient?.login({
 					maxTimeToLive: AUTH_MAX_TIME_TO_LIVE,
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,16 +1,13 @@
+import juno from '@junobuild/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { type UserConfig } from 'vite';
-import juno from '@junobuild/vite-plugin';
 
 export const defineConfig = ({ port }: { port?: number } = {}): UserConfig => {
 	return {
 		build: {
 			emptyOutDir: true
 		},
-		plugins: [
-			sveltekit(),
-			juno({container: true})
-		],
+		plugins: [sveltekit(), juno({ container: true })],
 		server: {
 			...(port !== undefined && { port })
 		},
