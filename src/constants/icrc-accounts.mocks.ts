fd62fbe6864faed09e4e9a24e70e999fe93fc245
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-03 10:09:37 +0200

    feat: icrc-21 actor

--- src/constants/icrc-accounts.mocks.ts
+++ src/constants/icrc-accounts.mocks.ts
@@ -1,6 +1,8 @@
 import {Principal} from '@dfinity/principal';
 import type {IcrcAccounts} from '../types/icrc-accounts';
 
+export const mockCanisterId = 'doked-biaaa-aaaar-qag2a-cai';
+
 export const mockPrincipalText = 'xlmdg-vkosz-ceopx-7wtgu-g3xmd-koiyc-awqaq-7modz-zf6r6-364rh-oqe';
 
 export const mockAccounts: IcrcAccounts = [
