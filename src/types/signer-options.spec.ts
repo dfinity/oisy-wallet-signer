e27e0d744a695b069055cd4d0467119a7fd6c968
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-04 10:58:06 +0200

    feat: owner (equals sender) identity

--- src/types/signer-options.spec.ts
+++ src/types/signer-options.spec.ts
@@ -1,91 +1,51 @@
-import {AnonymousIdentity, HttpAgent, ProxyAgent} from '@dfinity/agent';
+import {AnonymousIdentity} from '@dfinity/agent';
 import {Ed25519KeyIdentity} from '@dfinity/identity';
 import {describe} from 'vitest';
 import {SignerOptionsSchema} from './signer-options';
 
-vi.mock('@dfinity/agent', async (importOriginal) => {
-  return {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-    ...(await importOriginal<typeof import('@dfinity/agent')>()),
-    createSync: vi.fn()
-  };
-});
-
 describe('SignerOptions', () => {
   describe('Owner', () => {
-    const agent = HttpAgent.createSync();
-
     it('should validate a valid owner', () => {
       const identity = Ed25519KeyIdentity.generate();
 
       const validSignerOptions = {
-        owner: identity.getPrincipal(),
-        agent
+        owner: identity
       };
 
       expect(() => SignerOptionsSchema.parse(validSignerOptions)).not.toThrow();
     });
 
-    it('should throw an error for invalid principal', () => {
-      const invalidPrincipal = {id: 'not-a-principal'};
+    it('should throw an error for invalid identity', () => {
+      const invalidIdentity = {id: 'not-an-identity'};
 
       const invalidSignerOptions = {
-        owner: invalidPrincipal,
-        agent
+        owner: invalidIdentity
       };
 
       expect(() => SignerOptionsSchema.parse(invalidSignerOptions)).toThrow(
-        'The value provided is not a valid Principal.'
+        'The value provided is not a valid Identity.'
+      );
+    });
+
+    it('should throw an error for invalid principal', () => {
+      const invalidIdentity = {_principal: {id: 'not-a-principal'}};
+
+      const invalidSignerOptions = {
+        owner: invalidIdentity
+      };
+
+      expect(() => SignerOptionsSchema.parse(invalidSignerOptions)).toThrow(
+        'The value provided is not a valid Identity.'
       );
     });
 
     it('should throw an error for an anonymous Principal', () => {
       const invalidSignerOptions = {
-        owner: new AnonymousIdentity().getPrincipal(),
-        agent
+        owner: new AnonymousIdentity()
       };
 
       expect(() => SignerOptionsSchema.parse(invalidSignerOptions)).toThrow(
         'The Principal is anonymous and cannot be used.'
-      );
-    });
-  });
-
-  describe('Agent', () => {
-    const owner = Ed25519KeyIdentity.generate().getPrincipal();
-
-    it('should validate a valid HttpAgent', () => {
-      const agent = HttpAgent.createSync();
-
-      const validSignerOptions = {
-        owner,
-        agent
-      };
-
-      expect(() => SignerOptionsSchema.parse(validSignerOptions)).not.toThrow();
-    });
-
-    it('should validate a valid ProxyAgent', () => {
-      const agent = new ProxyAgent(() => undefined);
-
-      const validSignerOptions = {
-        owner,
-        agent
-      };
-
-      expect(() => SignerOptionsSchema.parse(validSignerOptions)).not.toThrow();
-    });
-
-    it('should throw an error for an invalid agent', () => {
-      const invalidAgent = {};
-
-      const invalidSignerOptions = {
-        owner,
-        agent: invalidAgent
-      };
-
-      expect(() => SignerOptionsSchema.parse(invalidSignerOptions)).toThrow(
-        'Invalid agent instance.'
       );
     });
   });
--- src/types/signer-options.ts
+++ src/types/signer-options.ts
@@ -1,39 +1,32 @@
-import {HttpAgent, ProxyAgent, type Agent} from '@dfinity/agent';
-import {Principal} from '@dfinity/principal';
+import type {Identity} from '@dfinity/agent';
+import {isNullish} from '@dfinity/utils';
 import {z} from 'zod';
 
-const PrincipalSchema = z.custom<Principal>((value: unknown) => {
+const IdentitySchema = z.custom<Identity>((value: unknown): boolean => {
+  if (isNullish(value)) {
+    return false;
+  }
+
   if (typeof value !== 'object') {
     return false;
   }
 
-  const {_isPrincipal} = value as unknown as {_isPrincipal: boolean | undefined};
-
-  if (_isPrincipal !== true) {
-    return false;
-  }
-
-  const {_arr} = value as unknown as {_arr: Uint8Array | undefined};
-  if (_arr === undefined) {
-    return false;
-  }
-
   try {
-    Principal.fromUint8Array(_arr);
+    (value as Identity).getPrincipal();
     return true;
   } catch (err: unknown) {
     return false;
   }
-}, 'The value provided is not a valid Principal.');
+}, 'The value provided is not a valid Identity.');
 
-const PrincipalNotAnonymousSchema = PrincipalSchema.refine(
-  (principal: Principal): boolean => {
-    return !principal.isAnonymous();
-  },
+const IdentityNotAnonymousSchema = IdentitySchema.refine(
+  (identity) => !identity.getPrincipal().isAnonymous(),
   {
     message: 'The Principal is anonymous and cannot be used.'
   }
 );
+
+export type IdentityNotAnonymous = z.infer<typeof IdentityNotAnonymousSchema>;
 
 export const SignerOptionsSchema = z.object({
   /**
@@ -42,19 +35,7 @@ export const SignerOptionsSchema = z.object({
    * When the signer is initialized, the owner should be signed in to the consumer dApp.
    * Upon signing out, it is up to the consumer to disconnect the signer.
    */
-  owner: PrincipalNotAnonymousSchema,
-
-  // TODO: instead of an agent, should the consumer only pas a "DEV" mode?
-  // The library can take care of creating the Anonymous agent for consent message, at least for now.
-
-  /**
-   * An agent that the signer can use to fetch the consent message during a canister call.
-   *
-   * This should be an instance of either `HttpAgent` or `ProxyAgent`.
-   */
-  agent: z.custom<Agent>((agent) => agent instanceof ProxyAgent || agent instanceof HttpAgent, {
-    message: 'Invalid agent instance.'
-  })
+  owner: IdentityNotAnonymousSchema
 });
 
 export type SignerOptions = z.infer<typeof SignerOptionsSchema>;
