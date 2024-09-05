abc9c341bfaa99a7d234988c528da107a7cdc606
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-05 17:09:41 +0200

    test: revert serial

--- e2e/accounts.spec.ts
+++ e2e/accounts.spec.ts
@@ -1,8 +1,6 @@
 import {testWithII} from '@dfinity/internet-identity-playwright';
 import {initTestSuite} from './utils/init.utils';
 
-testWithII.describe.configure({mode: 'serial'});
-
 const getPartyPage = initTestSuite();
 
 testWithII('should requests permissions and list accounts', async () => {
