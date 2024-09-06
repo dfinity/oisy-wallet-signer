c59a81a83b5e0a67776e4a78e6190255ac02879e
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-06 15:24:39 +0200

    feat: wip

--- src/api/signer.api.ts
+++ src/api/signer.api.ts
@@ -1,9 +1,10 @@
+import {defaultStrategy, pollForResponse} from '@dfinity/agent/lib/cjs/polling';
+import {Principal} from '@dfinity/principal';
 import type {IcrcCallCanisterRequestParams} from '../types/icrc-requests';
 import type {SignerOptions} from '../types/signer-options';
 import {Icrc21Canister} from './icrc21-canister.api';
 
 export class SignerApi extends Icrc21Canister {
-  // TODO: return result
   async call({
     owner,
     host,
@@ -13,10 +14,30 @@ export class SignerApi extends Icrc21Canister {
   } & SignerOptions): Promise<void> {
     const agent = await this.getAgent({host, owner});
 
-    await agent.call(canisterId, {
+    const {
+      requestId,
+      response: callResponse,
+      requestDetails
+    } = await agent.call(canisterId, {
       methodName,
       arg,
       effectiveCanisterId: canisterId
     });
+
+    // TODO: return result
+    console.log('RESULT:', requestId, callResponse, requestDetails);
+
+    if (callResponse.status === 202) {
+      const response = await pollForResponse(
+        agent,
+        Principal.fromText(canisterId),
+        requestId,
+          defaultStrategy(),
+      );
+
+      response.certificate
+
+      console.log('RESPONSE:', response);
+    }
   }
 }
