a791139e31e8bb2049511e2e305e2e2d4ebf67d6
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-10 13:06:07 +0200

    chore: playwright ci workers set to one (#198)

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,5 +28,6 @@ export default defineConfig({
       name: 'Google Chrome',
       use: {...devices['Desktop Chrome'], channel: 'chrome'}
     }
-  ]
+  ],
+  workers: process.env.CI ? 1 : undefined
 });
