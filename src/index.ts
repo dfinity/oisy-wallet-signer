dd2ed7a6b79f2956dd12f61afdef3a40cbaee55d
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-11 08:37:23 +0200

    feat: expose type origin

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export type * from './types/icrc-accounts';
 export type * from './types/icrc-requests';
 export type * from './types/icrc-responses';
 export type * from './types/icrc-standards';
+export type * from './types/post-message';
 export type * from './types/principal';
 export type * from './types/relying-party-options';
 export type * from './types/relying-party-requests';
