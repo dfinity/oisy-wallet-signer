00f8b6e637d14c6e4cd6ed2f92af5c47d9d9b773
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-17 08:21:54 +0200

    fix: redo valid subaccount length

--- src/types/icrc-accounts.ts
+++ src/types/icrc-accounts.ts
@@ -1,11 +1,25 @@
 import {z} from 'zod';
+import {base64ToUint8Array} from '../utils/base64.utils';
 import {IcrcBlob} from './blob';
 import {PrincipalTextSchema} from './principal';
+
+const IcrcSubaccountSchema = IcrcBlob.refine(
+  (value) => {
+    try {
+      return base64ToUint8Array(value).length === 32;
+    } catch (_err: unknown) {
+      return false;
+    }
+  },
+  {
+    message: 'Subaccount must be exactly 32 bytes long.'
+  }
+);
 
 export const IcrcAccountSchema = z
   .object({
     owner: PrincipalTextSchema,
-    subaccount: IcrcBlob.optional()
+    subaccount: IcrcSubaccountSchema.optional()
   })
   .strict();
 
