47cd4a5dd8141aa12fc5277ada9c1e1696a3e4c8
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-17 13:43:17 +0200

    feat: get ICP for demo and E2E

--- demo/src/wallet_frontend/src/routes/sign/+page.svelte
+++ demo/src/wallet_frontend/src/routes/sign/+page.svelte
@@ -2,11 +2,13 @@
 	import { notSignedIn } from '$core/derived/auth.derived';
 	import { Signer } from '@dfinity/oisy-wallet-signer/signer';
 	import UserId from '$core/components/UserId.svelte';
+    import Balance from '$core/components/Balance.svelte';
 	import ConfirmPermissions from '$lib/ConfirmPermissions.svelte';
 	import { authStore } from '$core/stores/auth.store';
 	import { isNullish } from '@dfinity/utils';
 	import ConfirmAccounts from '$lib/ConfirmAccounts.svelte';
 	import ConfirmConsentMessage from '$lib/ConfirmConsentMessage.svelte';
+    import GetICP from "$lib/GetICP.svelte";
 
 	let signer = $state<Signer | undefined>(undefined);
 
@@ -33,6 +35,10 @@
 </script>
 
 <UserId />
+
+<Balance />
+
+<GetICP />
 
 <ConfirmPermissions {signer} />
 
