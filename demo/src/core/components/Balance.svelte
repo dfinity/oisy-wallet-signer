9b856a41506d2e834e5c28ae9bad90bd4e6d68fa
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-17 13:43:31 +0200

    chore: format

--- demo/src/core/components/Balance.svelte
+++ demo/src/core/components/Balance.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-    import {createAgent, isNullish} from '@dfinity/utils';
+	import { createAgent, isNullish } from '@dfinity/utils';
 	import { AccountIdentifier, LedgerCanister } from '@dfinity/ledger-icp';
 	import { ICP_LEDGER_CANISTER_ID } from '../constants/app.constants';
 	import { formatE8sICP } from '../utils/icp.utils';
 	import { AnonymousIdentity } from '@dfinity/agent';
-    import type {Principal} from "@dfinity/principal";
-    import { authStore } from '../stores/auth.store';
+	import type { Principal } from '@dfinity/principal';
+	import { authStore } from '../stores/auth.store';
 
 	let balance = $state(0n);
 
 	const loadBalance = async (owner: Principal | undefined | null) => {
-        if (isNullish(owner)) {
-            balance = 0n;
-            return
-        }
+		if (isNullish(owner)) {
+			balance = 0n;
+			return;
+		}
 
-        const agent = await createAgent({
+		const agent = await createAgent({
 			identity: new AnonymousIdentity(),
 			host: 'http://localhost:4943',
 			fetchRootKey: true
@@ -26,12 +26,15 @@
 			canisterId: ICP_LEDGER_CANISTER_ID
 		});
 
-		balance = await accountBalance({ accountIdentifier: AccountIdentifier.fromPrincipal({principal: owner}), certified: false });
+		balance = await accountBalance({
+			accountIdentifier: AccountIdentifier.fromPrincipal({ principal: owner }),
+			certified: false
+		});
 	};
 
 	$effect(() => {
 		(async () => {
-            await loadBalance($authStore.identity.getPrincipal());
+			await loadBalance($authStore.identity.getPrincipal());
 		})();
 	});
 
