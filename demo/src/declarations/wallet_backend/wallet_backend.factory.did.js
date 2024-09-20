0cbad11433b31250695e633e930a1a1464f6f3b1
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-20 14:22:05 +0200

    chore: cleanup dfx backend and did leftovers (#227)
    * chore: cleanup dfx backend and did leftovers
    
    * chore: cleanup dfx backend and did leftovers

--- demo/src/declarations/wallet_backend/wallet_backend.factory.did.js
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-export const idlFactory = ({ IDL }) => {
-	return IDL.Service({ greet: IDL.Func([IDL.Text], [IDL.Text], ['query']) });
-};
-// @ts-ignore
-export const init = ({ IDL }) => {
-	return [];
-};
--- demo/src/wallet_backend/src/state.rs
+++
@@ -1,6 +0,0 @@
-use crate::wallet_types::State;
-use std::cell::RefCell;
-
-thread_local! {
-    pub static STATE: RefCell<State> = RefCell::default();
-}
