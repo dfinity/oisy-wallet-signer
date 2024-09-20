64409bc4ee6359bf6d0c351049a62c28af2331c5
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-20 15:34:15 +0200

    fix: copy paste

--- src/signer.ts
+++ src/signer.ts
@@ -101,7 +101,7 @@ export class Signer {
 
   // TODO: onbeforeupload, the signer should notify an error 4001 if and only if there is a pending request at the same time.
   // This means that the signer will have to keep track of its activity.
-  // See https://dfinity.slack.com/archives/D033WHSRZLH/p1726830103487179
+  // See https://github.com/dfinity/wg-identity-authentication/pull/212
 
   private readonly onMessageListener = (message: SignerMessageEvent): void => {
     void this.onMessage(message);
