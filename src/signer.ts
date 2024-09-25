2ac52956bc131daec7fafba959e4f02121718799
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-25 17:16:52 +0200

    feat: always return all scopes on permissions (#242)
    * feat: always return all scopes on permissions
    
    * feat: replace isNullish with explicit comparison

--- src/signer.ts
+++ src/signer.ts
@@ -296,13 +296,19 @@ export class Signer {
 
       const scopes = readSessionValidScopes({owner: owner.getPrincipal(), origin});
 
-      // TODO: extend scopes with delta of SIGNER_DEFAULT_SCOPES
-      //  i.e. relying party should always get the all list of permissions and those that have never been request should be set to ask_on_use
+      // The relying party should always receive the full list of permissions, and those that have never been requested or have expired should be provided as "ask_on_use".
+      const allScopes = [
+        ...(scopes ?? []),
+        ...SIGNER_DEFAULT_SCOPES.filter(
+          ({scope: {method: defaultMethod}}) =>
+            (scopes ?? []).find(({scope: {method}}) => method === defaultMethod) === undefined
+        )
+      ];
 
       notifyPermissionScopes({
         id,
         origin,
-        scopes: scopes ?? SIGNER_DEFAULT_SCOPES
+        scopes: allScopes
       });
       return {handled: true};
     }
