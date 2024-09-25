113a09546ac8d968a43361461406dbd16ba21c2f
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-25 13:25:36 +0200

    chore: format

--- src/icrc-wallet.ts
+++ src/icrc-wallet.ts
@@ -1,20 +1,19 @@
-import {RelyingParty} from "./relying-party";
-import type {Origin, RelyingPartyOptions} from "./index";
+import type {Origin, RelyingPartyOptions} from './index';
+import {RelyingParty} from './relying-party';
 
 export class IcrcWallet extends RelyingParty {
-
-    /**
-     * Establishes a connection with an ICRC Wallet.
-     *
-     * @override
-     * @static
-     * @param {RelyingPartyOptions} options - The options to initialize the ICRC Wallet signer.
-     * @returns {Promise<IcrcWallet>} A promise that resolves to an object, which can be used to interact with the ICRC Wallet when it is connected.
-     */
-    static async connect(options: RelyingPartyOptions): Promise<IcrcWallet> {
-        return await this.connectSigner({
-            options,
-            init: (params: {origin: Origin; popup: Window}) => new IcrcWallet(params)
-        });
-    }
+  /**
+   * Establishes a connection with an ICRC Wallet.
+   *
+   * @override
+   * @static
+   * @param {RelyingPartyOptions} options - The options to initialize the ICRC Wallet signer.
+   * @returns {Promise<IcrcWallet>} A promise that resolves to an object, which can be used to interact with the ICRC Wallet when it is connected.
+   */
+  static async connect(options: RelyingPartyOptions): Promise<IcrcWallet> {
+    return await this.connectSigner({
+      options,
+      init: (params: {origin: Origin; popup: Window}) => new IcrcWallet(params)
+    });
+  }
 }
