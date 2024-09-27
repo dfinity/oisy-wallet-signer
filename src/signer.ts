d44ba7872af64b8fbb1c70887130d90066cfeea6
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-27 16:53:06 +0200

    chore: eslint lib as Oisy Wallet rules (#252)

--- src/signer.ts
+++ src/signer.ts
@@ -69,6 +69,7 @@ import {
 export class Signer {
   readonly #signerOptions: SignerOptions;
 
+  // eslint-disable-next-line local-rules/use-option-type-wrapper
   #walletOrigin: string | undefined | null;
 
   #permissionsPrompt: PermissionsPrompt | undefined;
