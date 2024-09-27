431658ee3e8e454dbd0e6b5ec4477c33e0c28b02
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-27 08:05:56 +0200

    chore: redo

--- e2e/call-canister.spec.ts
+++ e2e/call-canister.spec.ts
@@ -3,7 +3,7 @@ import {initTestSuite} from './utils/init.utils';
 
 const getPartyPage = initTestSuite();
 
-testWithII.skip('should call canister to transfer some ICP', async () => {
+testWithII('should call canister to transfer some ICP', async () => {
   const partyPage = getPartyPage();
 
   await partyPage.approvePermissionsAccounts();
