992b92ade218e44276600b529cc2c2b4d426adda
David Dal Busco <david.dalbusco@dfinity.org>
2024-09-30 17:26:06 +0200

    feat: icrc1 transfer

--- src/icrc-wallet.ts
+++ src/icrc-wallet.ts
@@ -1,6 +1,5 @@
-import {IDL} from '@dfinity/candid';
-import {TransferParams} from '@dfinity/ledger-icrc';
-import {toTransferArg} from '@dfinity/ledger-icrc/dist/types/converters/ledger.converters';
+import {TransferParams, toTransferArg} from '@dfinity/ledger-icrc';
+import {TransferArgs} from './constants/icrc.idl.constants';
 import type {
   IcrcAccount,
   IcrcCallCanisterResult,
@@ -9,7 +8,7 @@ import type {
   RelyingPartyOptions
 } from './index';
 import {RelyingParty} from './relying-party';
-import {uint8ArrayToBase64} from './utils/base64.utils';
+import {encodeArg} from './utils/call.utils';
 
 export class IcrcWallet extends RelyingParty {
   /**
@@ -35,30 +34,12 @@ export class IcrcWallet extends RelyingParty {
     IcrcAccount,
     'owner'
   >): Promise<IcrcCallCanisterResult> => {
-    // TODO: this should be exposed by Candid IDL
-    const Subaccount = IDL.Vec(IDL.Nat8);
+    const rawArgs = toTransferArg(params);
 
-    const Tokens = IDL.Nat;
-
-    const Timestamp = IDL.Nat64;
-
-    const Account = IDL.Record({
-      owner: IDL.Principal,
-      subaccount: IDL.Opt(Subaccount)
+    const arg = encodeArg({
+      recordClass: TransferArgs,
+      rawArgs
     });
-
-    const TransferArg = IDL.Record({
-      to: Account,
-      fee: IDL.Opt(Tokens),
-      memo: IDL.Opt(IDL.Vec(IDL.Nat8)),
-      from_subaccount: IDL.Opt(Subaccount),
-      created_at_time: IDL.Opt(Timestamp),
-      amount: Tokens
-    });
-
-    const rawArg = toTransferArg(params);
-
-    const arg = uint8ArrayToBase64(new Uint8Array(IDL.encode([TransferArg], [rawArg])));
 
     return await this.call({
       params: {
