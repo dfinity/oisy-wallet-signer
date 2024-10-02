b8091431a64d93418637e6b622b73e936549a20c
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-02 09:04:15 +0200

    feat: create a utility around IDL.decode to decode results (#265)

--- src/utils/idl.utils.ts
+++ src/utils/idl.utils.ts
@@ -1,5 +1,5 @@
 import {IDL} from '@dfinity/candid';
-import {RecordClass} from '@dfinity/candid/lib/cjs/idl';
+import {RecordClass, VariantClass} from '@dfinity/candid/lib/cjs/idl';
 import {IcrcBlob} from '../types/blob';
 import {uint8ArrayToBase64} from './base64.utils';
 
@@ -7,6 +7,24 @@ export const encodeArg = <T>({
   recordClass,
   rawArgs
 }: {
-  recordClass: RecordClass;
+  recordClass: RecordClass | VariantClass;
   rawArgs: T;
 }): IcrcBlob => uint8ArrayToBase64(new Uint8Array(IDL.encode([recordClass], [rawArgs])));
+
+export const decodeResult = <T>({
+  recordClass,
+  reply
+}: {
+  recordClass: RecordClass | VariantClass;
+  reply: ArrayBuffer;
+}): T => {
+  const result = IDL.decode([recordClass], reply);
+
+  if (result.length !== 1) {
+    throw new Error('More than one object returned. This is unexpected.');
+  }
+
+  // We have to use another ugly type cast because IDL.decode does not accept generics. Additionally, the agent-js implementation does not provide any hints, as its decodeReturnValue relies on the type 'any,' which is bad practice.
+  const [response] = result as unknown as [T];
+  return response;
+};
