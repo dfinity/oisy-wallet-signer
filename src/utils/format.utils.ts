892cb057a766ef80b55d8ba74c94c2fcb09baf18
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-24 09:35:43 +0100

    feat: format amount with min one decimal

--- src/utils/format.utils.ts
+++ src/utils/format.utils.ts
@@ -2,7 +2,7 @@ export const formatAmount = ({amount, decimals}: {amount: bigint; decimals: numb
   const converted = Number(amount) / 10 ** decimals;
 
   // For ease of readability we want to display two decimals at least but, handle edge case where ledgers have none or 1 decimal set.
-  const minimumFractionDigits = decimals >= 2 ? 2 : decimals;
+  const minimumFractionDigits = decimals >= 1 ? 1 : decimals;
 
   return new Intl.NumberFormat('en-US', {
     minimumFractionDigits,
