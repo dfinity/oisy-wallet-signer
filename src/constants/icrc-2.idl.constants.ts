b91c19bb3162f5bfcb4877e84c98989692a5d851
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-30 07:24:44 +0100

    chore: fmt

--- src/constants/icrc-2.idl.constants.ts
+++ src/constants/icrc-2.idl.constants.ts
@@ -5,17 +5,17 @@ import {IDL} from '@dfinity/candid';
  */
 
 const Account = IDL.Record({
-    owner: IDL.Principal,
-    subaccount: IDL.Opt(IDL.Vec(IDL.Nat8))
+  owner: IDL.Principal,
+  subaccount: IDL.Opt(IDL.Vec(IDL.Nat8))
 });
 
 export const ApproveArgs = IDL.Record({
-    fee: IDL.Opt(IDL.Nat),
-    memo: IDL.Opt(IDL.Vec(IDL.Nat8)),
-    from_subaccount: IDL.Opt(IDL.Vec(IDL.Nat8)),
-    created_at_time: IDL.Opt(IDL.Nat64),
-    amount: IDL.Nat,
-    expected_allowance: IDL.Opt(IDL.Nat),
-    expires_at: IDL.Opt(IDL.Nat64),
-    spender: Account
+  fee: IDL.Opt(IDL.Nat),
+  memo: IDL.Opt(IDL.Vec(IDL.Nat8)),
+  from_subaccount: IDL.Opt(IDL.Vec(IDL.Nat8)),
+  created_at_time: IDL.Opt(IDL.Nat64),
+  amount: IDL.Nat,
+  expected_allowance: IDL.Opt(IDL.Nat),
+  expires_at: IDL.Opt(IDL.Nat64),
+  spender: Account
 });
