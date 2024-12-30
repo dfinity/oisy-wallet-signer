33db49cb20cc6b906e7ead73f3a8ce0ca93f9543
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-30 10:54:37 +0100

    chore: fmt

--- src/constants/signer.builders.constants.ts
+++ src/constants/signer.builders.constants.ts
@@ -1,4 +1,7 @@
-import {buildContentMessageIcrc1Transfer, buildContentMessageIcrc2Approve} from '../builders/signer.builders';
+import {
+  buildContentMessageIcrc1Transfer,
+  buildContentMessageIcrc2Approve
+} from '../builders/signer.builders';
 import {SignerBuilderFn, SignerBuilderMethods} from '../types/signer-builders';
 
 export const SIGNER_BUILDERS: Record<SignerBuilderMethods, SignerBuilderFn> = {
