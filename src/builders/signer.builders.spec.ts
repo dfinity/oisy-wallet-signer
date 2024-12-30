7c9566b02effaf153b1f71c29eb82c11066c50b8
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-30 14:34:47 +0100

    feat: icon warning

--- src/builders/signer.builders.spec.ts
+++ src/builders/signer.builders.spec.ts
@@ -334,7 +334,7 @@ ${encodeIcrcAccount({owner: owner.getPrincipal()})}
 **Requested withdrawal allowance:**
 3,200.00000001 TKN
 
-⚠️  The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
+⚠ The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
 
 **Approval fee:**
 0.0010033 TKN
@@ -374,7 +374,7 @@ ${encodeIcrcAccount({owner: owner.getPrincipal(), subaccount: subaccount})}
 **Requested withdrawal allowance:**
 3,200.00000001 TKN
 
-⚠️  The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
+⚠ The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
 
 **Approval fee:**
 0.0010033 TKN
@@ -414,7 +414,7 @@ ${encodeIcrcAccount({owner: owner.getPrincipal()})}
 **Requested withdrawal allowance:**
 3,200.00000001 TKN
 
-⚠️  The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
+⚠ The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
 
 **Approval fee:**
 0.0010033 TKN
@@ -513,7 +513,7 @@ ${encodeIcrcAccount({owner: owner.getPrincipal()})}
 **Requested withdrawal allowance:**
 3,200.00000001 TKN
 
-⚠️  The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
+⚠ The allowance will be set to 3,200.00000001 TKN independently of any previous allowance. Until this transaction has been executed the spender can still exercise the previous allowance (if any) to it's full amount.
 
 **Expiration date:**
 Mon, Dec 30, 2024, 08:30:16 UTC
