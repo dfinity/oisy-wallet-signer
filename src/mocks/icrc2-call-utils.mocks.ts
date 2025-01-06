ce5631731512cc9ab1140bb37e0ea413e1fd45fd
David Dal Busco <david.dalbusco@dfinity.org>
2025-01-06 08:57:00 +0100

    feat: support for icrc2 transfer from in IcrcWallet (#404)
    * feat: support for icrc2 transfer from in IcrcWallet
    
    * docs: jsdocs

--- src/mocks/icrc2-call-utils.mocks.ts
+++ src/mocks/icrc2-call-utils.mocks.ts
@@ -15,8 +15,14 @@ export const mockIcrc2LocalRelyingPartyPrincipal = Principal.fromText(
   '6ngla-7dqvy-l73ju-54jt5-a4gvm-itu47-4dpih-e2h55-vtbzn-24yem-sae'
 );
 
+export const mockIcrc2LocalWalletPrincipal = Principal.fromText(
+  'ze7im-h75ws-wmw6d-lhd3w-3honz-35peh-rirlb-l5l6w-it5j5-tqzrn-dqe'
+);
+
+// Approve mocks
+
 export const mockIcrc2ApproveLocalCallParams = {
-  sender: 'ze7im-h75ws-wmw6d-lhd3w-3honz-35peh-rirlb-l5l6w-it5j5-tqzrn-dqe',
+  sender: mockIcrc2LocalWalletPrincipal.toText(),
   method: 'icrc2_approve',
   canisterId: mockLedgerCanisterId,
   arg: 'RElETAZufW17bgFueGwCs7DawwNorYbKgwUCbAjG/LYCALqJ5cIEAqLelOsGAoLz85EMA9ijjKgNfZGcnL8NAN6n99oNA8uW3LQOBAEFAZBOAAAAgOHrFwAAAR1wrhf9pp3iZ9Bw1WInTn+Deg5NH72sw5brmCMkAgA='
@@ -32,3 +38,22 @@ export const mockIcrc2ApproveLocalCallResult: IcrcCallCanisterResult = {
 export const mockIcrc2ApproveLocalCallTime = new Date(Date.parse('2024-12-30T12:12:00.000Z'));
 
 export const mockIcrc2ApproveLocalBlockHeight = 8n;
+
+// Transfer from mocks
+
+export const mockIcrc2TransferFromLocalCallParams = {
+  ...mockIcrc2ApproveLocalCallParams,
+  method: 'icrc2_transfer_from',
+  arg: 'RElETAZte24AbAKzsNrDA2ithsqDBQFufW54bAf7ygECxvy2AgPhhcGUAgHqyoqeBAK6ieXCBAGC8/ORDATYo4yoDX0BBQEdcK4X/aad4mfQcNViJ05/g3oOTR+9rMOW65gjJAIAAAABHf20rMt4azj3bZ3NzvryHiiKwr6v1kT6ns4Zi0cCAAAAwPD1Cw=='
+};
+
+export const mockIcrc2TransferFromLocalCallResult: IcrcCallCanisterResult = {
+  certificate:
+    '2dn3omlzaWduYXR1cmVYMJFCwHeFc/bu+KqcTgJ51YpsztIDCLzG+D/DEiB5q7ehre5pezfBOkdwlsz3KyRfZ2R0cmVlgwGDAYIEWCDq18IVU+dMZh3PDmralOOBqSdmIZNvjDyanIyr9TfclIMBggRYIFghWA1zGC7YseiKGQRMlABq1h3wm6wnw9q+mN1ENdJ/gwJOcmVxdWVzdF9zdGF0dXODAYMBgwGCBFggAKQnJau/gOt6oTaRQtBZBxRr2aBU6pgmFtPcb2BhOlSDAYMBggRYIN2OQR4karXwjh4qsc0MOU6cMPYFZ/82y/9WnyXRBe3ugwJYIDIrGZBwLNmJJpYhvsatvU4VSKK0t/wXan4bhpQw4ZvqgwGDAkVyZXBseYIDWIlESURMCWsCvIoBfcX+0gEBawnRxJh8AsKR7LkCf7XamqMDA5TBx4kEBOuCqJcEBaHD6/0HBvCH5tsJB5PlvsgMf+uc29UPCGwCx+vE0AlxxJixtQ19bAHYu7KEDH1sAZuzvqYKfWwBi73ymwF9bAG/m7fwDX1sAaO7kYwKeGwBnLq2nAJ9AQAAIIMCRnN0YXR1c4IDR3JlcGxpZWSCBFggmsuVwna+Rf951EyGY8WFZu8UBkXPYiuuAHGBhni9ywSCBFggpX9Goh6jf9hl5+b7cypd+aH7xd5643a3vIlOZShrEOmCBFggeWudrZcOvy3SQA6+lK1EK08HX7DJk8tHKB4+HF/mwQ6DAYIEWCArDpAWFa9ktOHy/2f5mVY6QUDu5VT3tpwaYSHzShq5CoMCRHRpbWWCA0mhx8XD+oiNixg=',
+  contentMap:
+    '2dn3p2NhcmdYjkRJREwGbXtuAGwCs7DawwNorYbKgwUBbn1ueGwH+8oBAsb8tgID4YXBlAIB6sqKngQCuonlwgQBgvPzkQwE2KOMqA19AQUBHXCuF/2mneJn0HDVYidOf4N6Dk0fvazDluuYIyQCAAAAAR39tKzLeGs4922dzc768h4oisK+r9ZE+p7OGYtHAgAAAMDw9QtrY2FuaXN0ZXJfaWRKAAAAAAAAAAIBAW5pbmdyZXNzX2V4cGlyeRsYFjR9fsBoAGttZXRob2RfbmFtZXNpY3JjMl90cmFuc2Zlcl9mcm9tZW5vbmNlUMFWSiN+XO/9CtZkWdc3DsZscmVxdWVzdF90eXBlZGNhbGxmc2VuZGVyWB39tKzLeGs4922dzc768h4oisK+r9ZE+p7OGYtHAg=='
+};
+
+export const mockIcrc2TransferFromLocalCallTime = new Date(Date.parse('2024-12-31T08:05:00.000Z'));
+
+export const mockIcrc2TransferFromLocalBlockHeight = 32n;
