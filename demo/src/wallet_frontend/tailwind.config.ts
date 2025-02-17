48444755f301a39949217847cf5b51fd92ed33ba
David Dal Busco <david.dalbusco@dfinity.org>
2025-02-17 10:26:30 +0100

    feat: migrate demo to tailwind v4 (#469)
    * feat: migrate demo to tailwind v4
    
    * chore: redo as in main
    
    * chore: redo tailwind

--- demo/src/wallet_frontend/tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import { tailwindConfig } from '../../tailwind.config';
-
-export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', '../core/components/**/*.{html,js,svelte,ts}'],
-	...tailwindConfig
-};
