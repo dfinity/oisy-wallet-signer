96968dba33b822b5a52f4a9dfe5708a502b85970
David Dal Busco <david.dalbusco@dfinity.org>
2025-02-18 14:49:27 +0100

    chore: lint

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,28 +5,28 @@ import tailwindcss from '@tailwindcss/vite';
 import { type UserConfig } from 'vite';
 
 export const defineConfig = ({ port }: { port?: number } = {}): UserConfig => ({
-		build: {
-			emptyOutDir: true,
-			rollupOptions: {
-				plugins: [
-					inject({
-						modules: { Buffer: ['buffer', 'Buffer'] }
-					})
-				]
-			}
-		},
-		plugins: [sveltekit(), juno({ container: true }), tailwindcss()],
-		server: {
-			...(port !== undefined && { port })
-		},
-		optimizeDeps: {
-			esbuildOptions: {
-				define: {
-					global: 'globalThis'
-				}
-			}
-		},
-		worker: {
-			format: 'es'
+	build: {
+		emptyOutDir: true,
+		rollupOptions: {
+			plugins: [
+				inject({
+					modules: { Buffer: ['buffer', 'Buffer'] }
+				})
+			]
 		}
-	});
+	},
+	plugins: [sveltekit(), juno({ container: true }), tailwindcss()],
+	server: {
+		...(port !== undefined && { port })
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: 'globalThis'
+			}
+		}
+	},
+	worker: {
+		format: 'es'
+	}
+});
