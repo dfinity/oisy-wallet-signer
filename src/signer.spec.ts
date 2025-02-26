04c1da87310fc99bb261128cf732a4bd13553ab7
David Dal Busco <david.dalbusco@dfinity.org>
2025-02-26 09:38:21 +0100

    chore: lint

--- src/signer.spec.ts
+++ src/signer.spec.ts
@@ -1680,7 +1680,7 @@ describe('Signer', () => {
               });
             });
 
-            describe('Busy', async () => {
+            describe('Busy', () => {
               let messageEvent: MessageEvent;
 
               beforeEach(async () => {
