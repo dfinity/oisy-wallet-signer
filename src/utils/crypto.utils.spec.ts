2715cd682e60d78262ff09736ff9bd0e55a6ebd7
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-03-20 13:21:33 +0100

    Fixed formatting

--- src/utils/crypto.utils.spec.ts
+++ src/utils/crypto.utils.spec.ts
@@ -1,17 +1,17 @@
-import { describe, it, expect } from 'vitest';
-import { generateHash } from './crypto.utils';
+import {describe, expect, it} from 'vitest';
+import {generateHash} from './crypto.utils';
 
 describe('generateHash', () => {
   it('should return a 64-character hexadecimal string', async () => {
-    const params = { key1: 'value1', key2: 'value2' };
+    const params = {key1: 'value1', key2: 'value2'};
     const hash = await generateHash(params);
     expect(hash).toHaveLength(64);
     expect(hash).toMatch(/^[0-9a-f]+$/);
   });
 
   it('should produce the same hash regardless of key order', async () => {
-    const params1 = { key1: 'value1', key2: 'value2' };
-    const params2 = { key2: 'value2', key1: 'value1' };
+    const params1 = {key1: 'value1', key2: 'value2'};
+    const params2 = {key2: 'value2', key1: 'value1'};
 
     const hash1 = await generateHash(params1);
     const hash2 = await generateHash(params2);
@@ -20,8 +20,8 @@ describe('generateHash', () => {
   });
 
   it('should produce different hashes for different inputs', async () => {
-    const params1 = { key1: 'value1', key2: 'value2' };
-    const params2 = { key1: 'differentValue', key2: 'value2' };
+    const params1 = {key1: 'value1', key2: 'value2'};
+    const params2 = {key1: 'differentValue', key2: 'value2'};
 
     const hash1 = await generateHash(params1);
     const hash2 = await generateHash(params2);
