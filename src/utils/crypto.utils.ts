dabf9a955a859749248a65c1b09d52d28be3c145
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-03-26 14:46:07 +0100

    feat: added new generateHash util (#513)
    * feat: added new generateHash util
    
    * Fixed formatting
    
    * Added new types and fixed comments
    
    * Changed hes-string import
    
    * Removed hexString and hexString.spec
    
    * Fixed build
    
    * Added new complex value types to test for generateHash method
    
    * Updated test for Principal toMatch  hexRegex
    
    * Added tests for simple and complex value type to be eqaul
    
    ---------
    
    Co-authored-by: David Dal Busco <david.dalbusco@dfinity.org>

---
+++ src/utils/crypto.utils.ts
@@ -0,0 +1,10 @@
+import {jsonReplacer, uint8ArrayToHexString} from '@dfinity/utils';
+import {HexString} from '../types/hex-string';
+
+export const generateHash = async <T extends object>(params: T): Promise<HexString> => {
+  const jsonString = JSON.stringify(params, jsonReplacer);
+  const dataBuffer = new TextEncoder().encode(jsonString);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', dataBuffer);
+
+  return uint8ArrayToHexString(new Uint8Array(hashBuffer));
+};
