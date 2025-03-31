fbc537129926ff9706197a6849084d286f701f68
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-03-31 23:25:35 +0200

    Fixed build

--- src/agent/custom-transform-agent.ts
+++ src/agent/custom-transform-agent.ts
@@ -1,8 +1,10 @@
 import {Expiry, HttpAgentRequest, HttpAgentRequestTransformFn} from '@dfinity/agent';
 import {isNullish, nowInBigIntNanoSeconds, uint8ArrayToBase64} from '@dfinity/utils';
-import {IcrcCallCanisterRequestParams} from 'src/types/icrc-requests';
+import {IcrcCallCanisterRequestParams} from '../types/icrc-requests';
 import {HexString} from '../types/hex-string';
 import {generateHash} from '../utils/crypto.utils';
+
+const MAX_CACHE_SIZE = 50;
 
 /**
  * A custom transform function that processes the HTTP agent request.
@@ -16,6 +18,21 @@ import {generateHash} from '../utils/crypto.utils';
  */
 export const customAddTransform = (): HttpAgentRequestTransformFn => {
   const cache: Map<HexString, Expiry> = new Map();
+
+  /**
+   * Cleans up expired entries from the cache.
+   * This function iterates over the cache and removes entries where the expiry time has passed.
+   * The cleanup is triggered before each transformation to ensure the cache does not grow uncontrollably.
+   */
+  const cleanupExpiredCache = () => {
+    const currentNanoTime = nowInBigIntNanoSeconds();
+
+    for (const [key, expiry] of cache) {
+      if (expiry['_value'] < currentNanoTime) {
+        cache.delete(key);
+      }
+    }
+  };
 
   return async (request: HttpAgentRequest) => {
     const {canister_id, sender, method_name, arg, ingress_expiry, nonce} = request.body;
@@ -33,6 +50,10 @@ export const customAddTransform = (): HttpAgentRequestTransformFn => {
     };
 
     const hash = await generateHash(hashRequestData);
+
+    if (cache.size > MAX_CACHE_SIZE) {
+      cleanupExpiredCache();
+    }
 
     const cachedExpiry = cache.get(hash);
 
