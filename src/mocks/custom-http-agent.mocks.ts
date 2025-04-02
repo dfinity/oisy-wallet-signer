5730e17b98af5c98aa710d2c163abfc7624d40b5
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-02 08:17:24 +0200

    Removed unnecessary mock

--- src/mocks/custom-http-agent.mocks.ts
+++ src/mocks/custom-http-agent.mocks.ts
@@ -16,11 +16,6 @@ export const mockRequestPayload: Pick<
   method: mockRequestMethod
 };
 
-export const mockRequestPayloadWithNonce: Omit<IcrcCallCanisterRequestParams, 'sender'> = {
-  ...mockRequestPayload,
-  nonce: uint8ArrayToBase64(new Uint8Array([1, 2, 3]))
-};
-
 export const mockRequestDetails: CallRequest = {
   arg: new Uint8Array([68, 73, 68, 76, 6, 109, 123, 110, 0, 108]),
   canister_id: Principal.fromText(mockCanisterId),
