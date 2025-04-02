80f76220eaa2abd0a37b07aa2c245d7c019127b4
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-02 11:21:56 +0200

    Added space for formatting

--- src/agent/custom-http-agent.ts
+++ src/agent/custom-http-agent.ts
@@ -26,6 +26,7 @@ export class RequestError extends Error {}
 export class InvalidCertificateReplyError extends Error {}
 export class InvalidCertificateStatusError extends Error {}
 export class UndefinedRootKeyError extends Error {}
+
 export class CustomHttpAgent extends HttpAgentProvider {
   private constructor(agent: HttpAgent) {
     super(agent);
