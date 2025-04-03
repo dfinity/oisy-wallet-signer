b1d8e38bd87e0e4f7edfe7804c3be2f27504ebb4
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-03 13:12:34 +0200

    feat: refactored agent api get HttpAgent (#540)
    * feat: refactored agent api get HttpAgent
    
    * Fixed lint
    
    * Updated agent.api
    
    * Reverted agent mutation
    
    * Fixed formatting
    
    * fix minor comments
    
    * Reverted icrc and signer api

--- src/api/agent.api.ts
+++ src/api/agent.api.ts
@@ -1,16 +1,24 @@
 import {isNullish} from '@dfinity/utils';
-import {CustomHttpAgent} from '../agent/custom-http-agent';
+import {CustomHttpAgent} from '../agent/custom-http-agent'; // Corrected import path
+import {HttpAgentProvider} from '../agent/http-agent-provider';
 import {MAINNET_REPLICA_URL} from '../constants/core.constants';
 import type {SignerOptions} from '../types/signer-options';
 
 export abstract class AgentApi {
-  #agents: Record<string, CustomHttpAgent> | undefined = undefined;
+  #agents: Record<string, HttpAgentProvider> | undefined = undefined;
 
-  protected async getAgent({owner, ...rest}: SignerOptions): Promise<CustomHttpAgent> {
-    const key = owner.getPrincipal().toText();
+  private async getAgent({
+    options,
+    type
+  }: {
+    options: SignerOptions;
+    type: 'default' | 'custom';
+  }): Promise<HttpAgentProvider | CustomHttpAgent> {
+    const {owner} = options;
+    const key = `${owner.getPrincipal().toText()}_${type}`;
 
     if (isNullish(this.#agents) || isNullish(this.#agents[key])) {
-      const agent = await this.createAgent({owner, ...rest});
+      const agent = await this.createAgent({options, type});
 
       this.#agents = {
         ...(this.#agents ?? {}),
@@ -23,15 +31,46 @@ export abstract class AgentApi {
     return this.#agents[key];
   }
 
-  private async createAgent({owner: identity, host}: SignerOptions): Promise<CustomHttpAgent> {
+  private async createAgent({
+    options: {owner: identity, host},
+    type
+  }: {
+    options: SignerOptions;
+    type: 'default' | 'custom';
+  }): Promise<HttpAgentProvider | CustomHttpAgent> {
     const {hostname} = new URL(host ?? MAINNET_REPLICA_URL);
-
     const shouldFetchRootKey = ['localhost', '127.0.0.1'].includes(hostname);
-
-    return await CustomHttpAgent.create({
+    const createOptions = {
       identity,
       host: host ?? MAINNET_REPLICA_URL,
       shouldFetchRootKey
-    });
+    };
+
+    if (type === 'default') {
+      return await HttpAgentProvider.create(createOptions);
+    }
+    return await CustomHttpAgent.create(createOptions);
+  }
+
+  /**
+   *Returns a default `HttpAgentProvider` instance for the given signer options.
+   *This agent does not overwrite any default features provided by agent-js.
+   *
+   * @param {SignerOptions} options - The signer configuration including identity and host.
+   * @returns {Promise<HttpAgentProvider>} - A promise that resolves to a default agent instance.
+   */
+  protected async getDefaultAgent(options: SignerOptions): Promise<HttpAgentProvider> {
+    return (await this.getAgent({options, type: 'default'})) as HttpAgentProvider;
+  }
+
+  /**
+   * Returns a `CustomHttpAgent` instance for the given signer options.
+   * This agent uses custom transforms that are notably used to handle nonce in calls.
+   *
+   * @param {SignerOptions} options - The signer configuration including identity and host.
+   * @returns {Promise<CustomHttpAgent>} - A promise that resolves to a custom agent instance.
+   */
+  protected async getCustomAgent(options: SignerOptions): Promise<CustomHttpAgent> {
+    return (await this.getAgent({options, type: 'custom'})) as CustomHttpAgent;
   }
 }
