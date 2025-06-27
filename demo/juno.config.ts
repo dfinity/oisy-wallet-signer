e871399d6c5e4a9df4210785e078b83a670d0aba
David Dal Busco <david.dalbusco@dfinity.org>
2025-06-27 08:55:28 +0300

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- demo/juno.config.ts
+++ demo/juno.config.ts
@@ -1,12 +1,12 @@
-import {defineConfig} from '@junobuild/config';
+import { defineConfig } from '@junobuild/config';
 
 export default defineConfig({
-  satellite: {
-    ids: {
-      development: 'jx5yt-yyaaa-aaaal-abzbq-cai',
-    },
-    source: 'build',
-  },
+	satellite: {
+		ids: {
+			development: 'jx5yt-yyaaa-aaaal-abzbq-cai'
+		},
+		source: 'build'
+	},
 	emulator: {
 		satellite: {}
 	}
