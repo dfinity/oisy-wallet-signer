a225fae50e6ebbcacb3c56b8fc9240da602f9667
David Dal Busco <david.dalbusco@dfinity.org>
2025-07-24 16:36:36 +0200

    feat: replace agentjs encode copy with new cbor library
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- src/utils/agentjs-cbor-copy.utils.ts
+++ src/utils/agentjs-cbor-copy.utils.ts
@@ -13,14 +13,17 @@ export const decodeCallRequest = (contentMap: IcrcBlob): CallRequest => {
 
   // The decode function copied from agent-js is buggy or does not support decoding the ingress_expiry to BigInt or Expiry. It seems that the value is a BigNumber. That's why we have to strip it from the response and convert it manually.
   // It does not parse Principal neither.
-  const {ingress_expiry, canister_id, ...callRequestRest} = decode<CborCallRequest>(
-    uint8ToBuf(base64ToUint8Array(contentMap))
-  );
+  const {ingress_expiry, canister_id, sender, ...callRequestRest} = decode<
+    Omit<CborCallRequest, 'ingress_expiry'> & {ingress_expiry: {_value: BigNumber} | BigNumber}
+  >(uint8ToBuf(base64ToUint8Array(contentMap)));
 
   return {
     ...callRequestRest,
-    canister_id: Principal.fromUint8Array(canister_id),
+    canister_id: Principal.from(canister_id),
+    sender: Principal.from(sender).toUint8Array(),
     // There is no constructor or setter to create an agent-js Expiry from a bigint. Type which is expected by CallRequest. Given that we solely require the wrapped BigInt in this function, we can resolve the issue with an ugly cast.
-    ingress_expiry: BigInt(ingress_expiry.toFixed()) as unknown as Expiry
+    ingress_expiry: BigInt(
+      '_value' in ingress_expiry ? ingress_expiry._value.toFixed() : ingress_expiry.toFixed()
+    ) as unknown as Expiry
   } as CallRequest;
 };
