f48820cb1484dfef6d322a904ab9593eca000d76
David Dal Busco <david.dalbusco@dfinity.org>
2025-07-25 08:54:17 +0200

    feat: types
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- src/utils/agentjs-cbor-copy.utils.ts
+++ src/utils/agentjs-cbor-copy.utils.ts
@@ -6,16 +6,21 @@ import type {BigNumber} from 'bignumber.js';
 import type {IcrcBlob} from '../types/blob';
 
 export const decodeCallRequest = (contentMap: IcrcBlob): CallRequest => {
+  type IngressExpiryAgentJSv3 = {_value: bigint};
+  type IngressExpiryAgentJSv2 = BigNumber;
+
   type CborCallRequest = Omit<CallRequest, 'ingress_expiry' | 'canister_id'> & {
-    ingress_expiry: BigNumber;
+    ingress_expiry: IngressExpiryAgentJSv3 | IngressExpiryAgentJSv2;
     canister_id: Uint8Array;
   };
 
   // The decode function copied from agent-js is buggy or does not support decoding the ingress_expiry to BigInt or Expiry. It seems that the value is a BigNumber. That's why we have to strip it from the response and convert it manually.
   // It does not parse Principal neither.
-  const {ingress_expiry, canister_id, sender, ...callRequestRest} = decode<
-    Omit<CborCallRequest, 'ingress_expiry'> & {ingress_expiry: {_value: bigint} | BigNumber}
-  >(base64ToUint8Array(contentMap));
+  const {ingress_expiry, canister_id, sender, ...callRequestRest} = decode<CborCallRequest>(
+    base64ToUint8Array(contentMap)
+  );
+
+  console.log('callRequestRest: ', ingress_expiry);
 
   return {
     ...callRequestRest,
