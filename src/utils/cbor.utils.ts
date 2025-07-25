cf82b97c6c5ee35a9d1b60a5929d8020a63bcef5
David Dal Busco <david.dalbusco@dfinity.org>
2025-07-25 13:02:02 +0200

    docs: some comments
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- src/utils/cbor.utils.ts
+++ src/utils/cbor.utils.ts
@@ -3,6 +3,21 @@ import type {CborValue} from '@dfinity/cbor';
 import {nonNullish} from '@dfinity/utils';
 import {PrincipalObjSchema} from '../types/principal';
 
+/**
+ * A replacer function used to serialize specific fields of the contentMap.
+ *
+ * This function ensures:
+ * - `ingress_expiry` is converted from an `Expiry` object to its internal `bigint` representation.
+ * - `sender` and `canister_id` fields are converted from `Principal` objects to `Uint8Array`.
+ *
+ * These transformations maintain compatibility with the existing decoder in agent-js v2
+ * while conforming to the IC spec for CBOR encoding.
+ *
+ * @template T - The underlying type of the CBOR value.
+ * @param {CborValue<T>} [value] - The value to possibly transform.
+ * @param {string} [key] - The key associated with the value during object traversal.
+ * @returns {CborValue<T>} The transformed value if applicable, or the original value.
+ */
 // eslint-disable-next-line local-rules/prefer-object-params
 export const contentMapReplacer = <T>(value?: CborValue<T>, key?: string): CborValue<T> => {
   if (key === 'ingress_expiry' && nonNullish(value) && value instanceof Expiry) {
