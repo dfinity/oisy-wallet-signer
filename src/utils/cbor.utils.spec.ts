983807165247e88e75ef443ab739d1ae74e75024
David Dal Busco <david.dalbusco@dfinity.org>
2025-07-25 14:32:54 +0200

    test: keys
    Signed-off-by: David Dal Busco <david.dalbusco@dfinity.org>

--- src/utils/cbor.utils.spec.ts
+++ src/utils/cbor.utils.spec.ts
@@ -9,6 +9,7 @@ describe('cbor.utils', () => {
       const expiry = new Expiry(5 * 60 * 1000);
       const result = contentMapReplacer(expiry, 'ingress_expiry');
 
+      expect(result instanceof Expiry).toBeFalsy();
       expect(result).toBe((expiry as unknown as {_value: bigint})._value);
     });
 
@@ -46,6 +47,57 @@ describe('cbor.utils', () => {
       const result = contentMapReplacer(undefined, 'sender');
 
       expect(result).toBe(undefined);
+    });
+
+    it('returns original value if undefined key', () => {
+      const value = 'a value';
+      const result = contentMapReplacer(value, undefined);
+
+      expect(result).toBe(value);
+    });
+
+    it('returns original Principal if undefined key', () => {
+      const principal = Principal.fromText(mockPrincipalText);
+      const result = contentMapReplacer(principal, undefined);
+
+      expect(result instanceof Principal).toBeTruthy();
+      expect((result as Principal).toText()).toBe(principal.toText());
+    });
+
+    it('returns original Principal if misspelled sender key', () => {
+      const principal = Principal.fromText(mockPrincipalText);
+      const result = contentMapReplacer(principal, 'sendder');
+
+      expect(result instanceof Principal).toBeTruthy();
+      expect((result as Principal).toText()).toBe(principal.toText());
+    });
+
+    it('returns original Principal if misspelled canister_id key', () => {
+      const principal = Principal.fromText(mockPrincipalText);
+      const result = contentMapReplacer(principal, 'canister_idd');
+
+      expect(result instanceof Principal).toBeTruthy();
+      expect((result as Principal).toText()).toBe(principal.toText());
+    });
+
+    it('returns original Expiry if undefined key', () => {
+      const expiry = new Expiry(5 * 60 * 1000);
+      const result = contentMapReplacer(expiry, undefined);
+
+      expect(result instanceof Expiry).toBeTruthy();
+      expect((result as unknown as {_value: bigint})._value).toBe(
+        (expiry as unknown as {_value: bigint})._value
+      );
+    });
+
+    it('returns original Expiry if misspelled ingress_expiry key', () => {
+      const expiry = new Expiry(5 * 60 * 1000);
+      const result = contentMapReplacer(expiry, 'ingress_expiryy');
+
+      expect(result instanceof Expiry).toBeTruthy();
+      expect((result as unknown as {_value: bigint})._value).toBe(
+        (expiry as unknown as {_value: bigint})._value
+      );
     });
   });
 });
