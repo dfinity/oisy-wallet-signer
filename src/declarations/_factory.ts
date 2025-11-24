c0a9f2131d047c5816c087e223bf05ff1e7a04e2
David Dal Busco <david.dalbusco@dfinity.org>
2025-11-24 16:26:31 +0100

    refactor: move DID and IDL auto-generated declarations in a subfolder (#733)

--- src/declarations/_factory.ts
+++ src/declarations/_factory.ts
@@ -1,10 +1,10 @@
-import {idlFactory as idlFactoryIcrc1} from './icrc-1.idl';
-import {idlFactory as idlFactoryIcrc2} from './icrc-2.idl';
-import {idlFactory as idlFactoryIcrc21} from './icrc-21.idl';
+import {idlFactory as idlFactoryIcrc1} from './icrc/icrc-1.idl';
+import {idlFactory as idlFactoryIcrc2} from './icrc/icrc-2.idl';
+import {idlFactory as idlFactoryIcrc21} from './icrc/icrc-21.idl';
 
-import type {_SERVICE as Icrc1Actor} from './icrc-1';
-import type {_SERVICE as Icrc2Actor} from './icrc-2';
-import type {_SERVICE as Icrc21Actor} from './icrc-21';
+import type {_SERVICE as Icrc1Actor} from './icrc/icrc-1';
+import type {_SERVICE as Icrc2Actor} from './icrc/icrc-2';
+import type {_SERVICE as Icrc21Actor} from './icrc/icrc-21';
 
 export {
   idlFactoryIcrc1,
