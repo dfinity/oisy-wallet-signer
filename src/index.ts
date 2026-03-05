73304c9b1120391e0905844412995b648e84af9c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-05 15:26:32 +0100

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ export type * from './types/icrc-requests';
 export type * from './types/icrc-responses';
 export type * from './types/icrc-standards';
 export type * from './types/post-message';
-// Must be a value export so the errors can be used with `instanceof`.
 export * from './types/relying-party-errors';
 export type * from './types/relying-party-options';
 export type * from './types/relying-party-requests';
